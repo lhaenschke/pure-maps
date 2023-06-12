8f64618273ea8d7709569984c04572d002aae384
lhaenschke <leon.haenschke@stud.hs-bochum.de>
2023-06-12 13:07:39 +0200

    Changed for Testing

--- poor/timetables.py
+++ poor/timetables.py
@@ -55,14 +55,22 @@ class TimetableManager:
         self.trains = sorted(self.trains, key=lambda x: x.dep_time)
 
     def get_trains(self):
-        return [(
-            train.type,
-            train.name,
-            train.dep_time_hh,
-            train.dep_time_mm,
-            train.track,
-            train.next_stops
+        return [dict(
+            type=train.type,
+            name=train.name,
+            dep_time_hh=train.dep_time_hh,
+            dep_time_mm=train.dep_time_mm,
+            track=train.track,
+            next_stops=train.next_stops,
         ) for train in self.trains]
+        # return [(
+        #     train.type,
+        #     train.name,
+        #     train.dep_time_hh,
+        #     train.dep_time_mm,
+        #     train.track,
+        #     train.next_stops
+        # ) for train in self.trains]
 
     def __get_eva_number__(self, latitude: str, longitude: str) -> str:
         conn = http.client.HTTPSConnection("apis.deutschebahn.com")
