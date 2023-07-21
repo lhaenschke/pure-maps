d4762cd7220c5839a59270a6b4a1de4b7d8dfafc
lhaenschke <leon.haenschke@stud.hs-bochum.de>
2023-07-21 10:54:07 +0200

    Changed for Testing

--- src/trainconnection.cpp
+++ src/trainconnection.cpp
@@ -97,7 +97,7 @@ KPublicTransport::LocationRequest TrainConnection::createLocationRequest(const Q
     std::vector<QJsonObject> jsonObjects;
     KPublicTransport::LocationReply *reply = manager.queryLocation(req);
 
-    std::cout << reply.errorString() << std::endl;
+    std::cout << reply->errorString() << std::endl;
 
     const std::vector<KPublicTransport::Location> &resultsArray = reply->result();
 
