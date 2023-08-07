63573ce0662dd0692f92d1eb6288f5f942931df7
lhaenschke <leon.haenschke@stud.hs-bochum.de>
2023-08-07 17:36:25 +0200

    Changed for Routensuche-Testing

--- src/trainconnection.cpp
+++ src/trainconnection.cpp
@@ -159,9 +159,9 @@ QVariant TrainConnection::getJourneyBetweenLocations(const QString &fromLocation
     
     std::cout << "IdLoading: " << queryModel.isLoading() << std::endl;
 
-    for (auto result: queryModel.journeys()) {
-        std::cout << "Test" << std::endl;
-    }
+    // for (auto result: queryModel.journeys()) {
+    //     std::cout << "Test" << std::endl;
+    // }
 
 }
 
